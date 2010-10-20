475fd3e3fc129c0309387443f4733f96d86c5d79
nunofaria11 <nunofaria11@99c9b96b-6dd2-4da9-bfb5-3db95630f279>
2010-10-20 12:36:31 +0000

    Prototype of random graph generation using the Erdos-Renyi models: GNM (M = #edges) and GNP (p = connectoin probability)

---
+++ Graphs/src/Utilities/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+/**
+ *
+ * @author nuno
+ */
+public final class Constants {
+
+    public static final int MAX_WEIGHT = 20;
+    public static final int MAX_VERTEX_NUM = 1000;
+    public static final int MAX_EDGE_NUM = posibleEdgesNum(MAX_VERTEX_NUM);
+
+    public static int posibleEdgesNum(int v) {
+        return (v * (v - 1)) / 2;
+    }
+}
